24a27e6893c511ea6bd7e85feda553791a0835ec
Pau Giner <pau.giner@gmail.com>
2013-03-22 11:35:50 +0100

    Localisation info

---
+++ lang/extractor.js
@@ -0,0 +1,34 @@
+'{"n-mainpage-description": "' + $("#n-mainpage-description a,#n-mainpage a").text() + '",\n' +
+'"n-contents": "' + $("#n-contents a").text() + '",\n' +
+'"n-featuredcontent": "' + $("#n-featuredcontent a").text() + '",\n' +
+'"n-currentevents": "' + $("#n-currentevents a").text() + '",\n' +
+'"n-randompage": "' + $("#n-randompage a").text() + '",\n' +
+'"n-sitesupport": "' + $("#n-sitesupport a").text() + '",\n' +
+'"p-interaction": "' + $("#p-interaction h3 a").text() + '",\n' +
+'"n-help": "' + $("#n-help a").text() + '",\n' +
+'"n-aboutsite": "' + $("#n-aboutsite a").text() + '",\n' +
+'"n-portal": "' + $("#n-portal a").text() + '",\n' +
+'"n-recentchanges": "' + $("#n-recentchanges a").text() + '",\n' +
+'"n-contact": "' + $("#n-contact a").text() + '",\n' +
+'"p-tb": "' + $("#p-tb h3 a").text() + '",\n' +
+'"t-whatlinkshere": "' + $("t-whatlinkshere a").text() + '",\n' +
+'"t-recentchangeslinked": "' + $("#t-recentchangeslinked a").text() + '",\n' +
+'"t-upload": "' + $("#t-upload a").text() + '",\n' +
+'"t-specialpages": "' + $("#t-specialpages a").text() + '",\n' +
+'"t-permalink": "' + $("#t-permalink a").text() + '",\n' +
+'"t-info": "' + $("#t-info a").text() + '",\n' +
+'"t-cite": "' + $("#t-cite a").text() + '",\n' +
+'"p-coll-print_export": "' + $("#p-coll-print_export h3 a").text() + '",\n' +
+'"coll-create_a_book": "' + $("#coll-create_a_book a").text() + '",\n' +
+'"coll-download-as-rl": "' + $("#coll-download-as-rl a").text() + '",\n' +
+'"t-print": "' + $("#t-print a").text() + '",\n' +
+'"p-lang": "' + $("#p-lang h3 a").text() + '",\n' +
+'"ca-nstab-main": "' + $("#ca-nstab-main a").text() + '",\n' +
+'"ca-talk": "' + $("#ca-talk a").text() + '",\n' +
+'"ca-view": "' + $("#ca-view a").text() + '",\n' +
+'"ca-viewsource": "' + $("#ca-viewsource a").text() + '",\n' +
+'"ca-history": "' + $("#ca-history a").text() + '",\n' +
+'"pt-createaccount": "' + $("#pt-createaccount a").text() + '",\n' +
+'"pt-login": "' + $("#pt-login a").text() + '",\n' +
+'"sitename": "' + $("title").text().split("-")[1].split(",")[0] + '",\n' +
+'"motto": "' + $("title").text().split("-")[1].split(",")[1] + '"}' 
